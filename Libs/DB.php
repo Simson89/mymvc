35b7c044e2bdc90a2af5e8a2c6720de7be57ab90
Mateo <Mateo@Mateo-PC>
2013-12-12 19:15:11 +0100

    Dodałem wyjątek w obsłudze połączenia z bazą danych

--- Libs/DB.php
+++ Libs/DB.php
@@ -5,10 +5,17 @@ class DB extends PDO{
     private $db;
 
     function __construct() {
+        try{
         parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
         //$this->db = new PDO('mysql:host=localhost;dbname=mymvc;charset=utf8', 'root', '');
+       return $this->db;
+        }
         
-        return $this->db;
+ catch (PDOException $msg)
+ {
+            echo $msg='Błąd połączenia z bazą danych!';
+ }
+        
     }
 
     
